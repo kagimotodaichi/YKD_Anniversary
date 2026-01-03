3b810ecba4bd8a450fe6ee4258630c8d8ad2b425
daichi kagimoto <daichi.kagimoto@icloud.com>
2026-01-03 23:21:03 +0900

    Initial Commit

---
+++ YKD_Anniversary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YKD_Anniversary
+//
+//  Created by 鍵本大地 on 2026/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YKD_Anniversary/YKD_AnniversaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  YKD_AnniversaryApp.swift
+//  YKD_Anniversary
+//
+//  Created by 鍵本大地 on 2026/01/03.
+//
+
+import SwiftUI
+
+@main
+struct YKD_AnniversaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
